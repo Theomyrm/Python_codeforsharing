7aa9a794f4d5474e1167403feb9c4199542d3f8c
Theomyrm <73954335+Theomyrm@users.noreply.github.com>
2021-11-27 01:20:54 -0600

    Create Turtle_Code_Collection.py

---
+++ Collections/Turtle/Turtle_Code_Collection.py
@@ -0,0 +1,52 @@
+"""                 TURTLE CODE COLLECTION                   """
+#    """"""""""""""""""""""""""""""""""""""""""""""""""""""    #
+"""                                                          """ 
+"""
+                   Turtle code collection:
+
+              A personal collection of turtle
+         code for all purposes. Feel free to take 
+                  any code from here.
+                                        T. Myrm
+"""
+
+
+  ## * - * - ## * - * - ## Libraries ## * - * - ## * - * ##
+
+import turtle as t                 ##  Our guy  ## 
+from time import sleep     ##  Works a lot. Everyone knows  ##
+import math as m           ##  Need this for juicy stuff    ##
+import random as rnd       ##  Everything is chaos          ##
+
+  ## * - * - ## * - * - ## * - * - ## * - * - ## * - * - ##
+
+
+
+        ########## Creating turtle setups ##########
+        
+        
+  ## Windows; lets make windows with pride and joy!, it's ##
+  ## better to define our workspace from the beginning.   ##
+  
+  def makeWindow():
+      window = t.Screen()    ## The most basic configuration
+      window.bgcolor('pink') ## Show the world you fashion
+
+
+""" /-\  /-\  /-\  /-\    Test zone     /-\  /-\  /-\  /-\ """
+  
+    ## Just put in there the functions you want to test ##
+    ## At your own risk.                                ##
+  
+makeWindow()
+
+
+
+
+
+
+
+
+
+
+
